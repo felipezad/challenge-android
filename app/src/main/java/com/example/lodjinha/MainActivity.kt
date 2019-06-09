99554dbbefacc442a7346bb27185e377fd1f70b9
Felipe Zadinello <felipezad@gmail.com>
2019-06-09 10:29:37 -0300

    Removed unused imports

--- app/src/main/java/com/example/lodjinha/MainActivity.kt
+++ app/src/main/java/com/example/lodjinha/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.lodjinha
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
