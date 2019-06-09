b31fb16e24759964c46bfd25a940c0ef30ef98ca
Felipe Zadinello <felipezad@gmail.com>
2019-06-09 19:36:56 -0300

    Add Use cases from Main View

--- app/src/main/java/com/example/lodjinha/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/lodjinha/presentation/MainViewModel.kt
@@ -4,6 +4,11 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.lodjinha.domain.banner.Banner
+import com.example.lodjinha.domain.banner.GetBannerUseCase
+import com.example.lodjinha.domain.category.Category
+import com.example.lodjinha.domain.category.GetProductCategoryListUseCase
+import com.example.lodjinha.domain.product.GetMostProductsSoldListUseCase
 import com.example.lodjinha.domain.product.GetProductListUseCase
 import com.example.lodjinha.domain.product.Product
 import io.reactivex.disposables.CompositeDisposable
@@ -11,8 +16,14 @@ import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val getProductListUseCase: GetProductListUseCase,
+    private val getBannerUseCase: GetBannerUseCase,
+    private val getProductCategoryListUseCase: GetProductCategoryListUseCase,
+    private val getMostProductsSoldListUseCase: GetMostProductsSoldListUseCase,
     private val rxSchedulers: RxSchedulers,
-    private val productList: MutableLiveData<List<Product>> = MutableLiveData()
+    val productList: MutableLiveData<List<Product>> = MutableLiveData(),
+    val mostProductsSoldList: MutableLiveData<List<Product>> = MutableLiveData(),
+    val banner: MutableLiveData<List<Banner>> = MutableLiveData(),
+    val productCategories: MutableLiveData<List<Category>> = MutableLiveData()
 ) : ViewModel() {
 
 
@@ -21,15 +32,58 @@ class MainViewModel @Inject constructor(
     private fun handleProductList(result: GetProductListUseCase.Result) {
         when (result) {
             is GetProductListUseCase.Result.Success -> {
-//                productList.postValue(result.productList)
+                productList.postValue(result.productList)
 
-                val productList1 = result.productList
-                Log.d("success", productList1.toString())
             }
             is GetProductListUseCase.Result.Failure -> {
                 Log.d("error", "Error")
             }
             is GetProductListUseCase.Result.Loading -> {
+                Log.d("loading", "Loading")
+            }
+        }
+    }
+
+    private fun handleMostProductsSoldList(result: GetMostProductsSoldListUseCase.Result) {
+        when (result) {
+            is GetMostProductsSoldListUseCase.Result.Success -> {
+                mostProductsSoldList.postValue(result.productList)
+
+            }
+            is GetMostProductsSoldListUseCase.Result.Failure -> {
+                Log.d("error", "Error")
+            }
+            is GetMostProductsSoldListUseCase.Result.Loading -> {
+                Log.d("loading", "Loading")
+            }
+        }
+    }
+
+    private fun handleBanner(result: GetBannerUseCase.Result) {
+        when (result) {
+            is GetBannerUseCase.Result.Success -> {
+                banner.postValue(result.bannerList)
+
+            }
+            is GetBannerUseCase.Result.Failure -> {
+                Log.d("error", "Error")
+            }
+            is GetBannerUseCase.Result.Loading -> {
+                Log.d("loading", "Loading")
+            }
+        }
+    }
+
+    private fun handleProductCategory(result: GetProductCategoryListUseCase.Result) {
+        when (result) {
+            is GetProductCategoryListUseCase.Result.Success -> {
+                productCategories.postValue(result.categoryList)
+
+            }
+            is GetProductCategoryListUseCase.Result.Failure -> {
+                Log.d("error", "Error")
+            }
+            is GetProductCategoryListUseCase.Result.Loading -> {
                 Log.d("loading", "Loading")
             }
         }
@@ -45,12 +99,45 @@ class MainViewModel @Inject constructor(
         )
     }
 
+    fun getMostProductsSoldList() {
+        disposables.add(
+            getMostProductsSoldListUseCase
+                .execute()
+                .subscribeOn(rxSchedulers.ioThread)
+                .observeOn(rxSchedulers.androidMainThread)
+                .subscribe(this::handleMostProductsSoldList)
+        )
+    }
+
+    fun getBanner() {
+        disposables.add(
+            getBannerUseCase
+                .execute()
+                .subscribeOn(rxSchedulers.ioThread)
+                .observeOn(rxSchedulers.androidMainThread)
+                .subscribe(this::handleBanner)
+        )
+    }
+
+    fun getProductCategory() {
+        disposables.add(
+            getProductCategoryListUseCase
+                .execute()
+                .subscribeOn(rxSchedulers.ioThread)
+                .observeOn(rxSchedulers.androidMainThread)
+                .subscribe(this::handleProductCategory)
+        )
+    }
+
     fun destroy() {
         disposables.clear()
     }
 
     class MainViewModelFactory @Inject constructor(
         private val getProductListUseCase: GetProductListUseCase,
+        private val getBannerUseCase: GetBannerUseCase,
+        private val getProductCategoryListUseCase: GetProductCategoryListUseCase,
+        private val getMostProductsSoldListUseCase: GetMostProductsSoldListUseCase,
         private val rxSchedulers: RxSchedulers
     ) : ViewModelProvider.Factory {
 
@@ -58,6 +145,9 @@ class MainViewModel @Inject constructor(
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return MainViewModel(
                 getProductListUseCase = getProductListUseCase,
+                getBannerUseCase = getBannerUseCase,
+                getProductCategoryListUseCase = getProductCategoryListUseCase,
+                getMostProductsSoldListUseCase = getMostProductsSoldListUseCase,
                 rxSchedulers = rxSchedulers
             ) as T
         }
