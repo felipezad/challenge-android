df6bd096606fc98c4a8e96487de2ecb810f9b664
Felipe Zadinello <felipezad@gmail.com>
2019-06-09 12:41:47 -0300

    Move activity to Presentation package

--- app/src/main/java/com/example/lodjinha/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lodjinha/presentation/MainActivity.kt
@@ -1,12 +1,21 @@
-package com.example.lodjinha
+package com.example.lodjinha.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lodjinha.R
+import com.example.lodjinha.data.dagger.DaggerLodjinhaServiceComponent
+import com.example.lodjinha.data.dagger.LodjinhaServiceComponent
+import com.example.lodjinha.data.remote.LodjinhaService
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ComponentProvider<LodjinhaServiceComponent> {
 
+    override val component: LodjinhaServiceComponent by lazy {
+        DaggerLodjinhaServiceComponent
+            .factory()
+            .create()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
